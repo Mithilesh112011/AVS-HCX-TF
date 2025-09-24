5c62e6de568545b1727abc22504ec8a6e11e1a23
Mithilesh <mithilesh.thakur@ahead.com>
2025-09-24 17:23:22 +0530

    added few resources in monitor

--- monitoring.tf
+++ monitoring.tf
@@ -18,3 +18,129 @@ resource "azurerm_monitor_metric_alert" "avs_cpu" {
     }
   }
 }
+
+resource "azurerm_monitor_metric_alert" "avs_CPUCritical" {
+  name                = "CPUCritical-metricalert"
+  resource_group_name = azurerm_resource_group.privatecloud.name
+  scopes              = [module.avs_privatecloud.privatecloud_id]
+  description         = "CPU Critical  alert per cluster"
+
+  criteria {
+    metric_namespace = "Microsoft.AVS/privateClouds"
+    metric_name      = "EffectiveCpuAverage"
+    Aggregation      = "Average"
+    operator         = "GreaterThan"
+    threshold        = 95
+
+    dimension {
+      name     = "clustername"
+      operator = "Include"
+      values   = ["*"]
+    }
+  }
+}
+
+resource "azurerm_monitor_metric_alert" "avs_DiskUsedPercentage" {
+  name                = "DiskUsedPercentage-metricalert"
+  resource_group_name = azurerm_resource_group.privatecloud.name
+  scopes              = [module.avs_privatecloud.privatecloud_id]
+  description         = "Disk Used Percentage alert per cluster"
+
+  criteria {
+    metric_namespace = "Microsoft.AVS/privateClouds"
+    metric_name      = "DiskUsedPercentage"
+    Aggregation      = "Average"
+    operator         = "GreaterThan"
+    threshold        = 75
+
+    dimension {
+      name     = "dsname"
+      operator = "Include"
+      values   = ["*"]
+    }
+  }
+}
+
+resource "azurerm_monitor_metric_alert" "avs_Memory" {
+  name                = "Memory-metricalert"
+  resource_group_name = azurerm_resource_group.privatecloud.name
+  scopes              = [module.avs_privatecloud.privatecloud_id]
+  description         = "Memory alert per cluster"
+
+  criteria {
+    metric_namespace = "Microsoft.AVS/privateClouds"
+    metric_name      = "UsageAverage"
+    Aggregation      = "Average"
+    operator         = "GreaterThan"
+    threshold        = 80
+
+    dimension {
+      name     = "clustername"
+      operator = "Include"
+      values   = ["*"]
+    }
+  }
+}
+
+resource "azurerm_monitor_metric_alert" " avs_MemoryCritical" {
+  name                = "MemoryCritical-metricalert"
+  resource_group_name = azurerm_resource_group.privatecloud.name
+  scopes              = [module.avs_privatecloud.privatecloud_id]
+  description         = "Memory Critical alert per cluster"
+
+  criteria {
+    metric_namespace = "Microsoft.AVS/privateClouds"
+    metric_name      = "UsageAverage"
+    Aggregation      = "Average"
+    operator         = "GreaterThan"
+    threshold        = 95
+
+    dimension {
+      name     = "clustername"
+      operator = "Include"
+      values   = ["*"]
+    }
+  }
+}
+
+resource "azurerm_monitor_metric_alert" "avs_storage   " {
+  name                = "storage-metricalert"
+  resource_group_name = azurerm_resource_group.privatecloud.name
+  scopes              = [module.avs_privatecloud.privatecloud_id]
+  description         = "storage alert per cluster"
+
+  criteria {
+    metric_namespace = "Microsoft.AVS/privateClouds"
+    metric_name      = "DiskUsedPercentage"
+    Aggregation      = "Average"
+    operator         = "GreaterThan"
+    threshold        = 70
+
+    dimension {
+      name     = "dsname"
+      operator = "Include"
+      values   = ["*"]
+    }
+  }
+}
+
+resource "azurerm_monitor_metric_alert" "avs_UsageAverage"  {
+  name                = "UsageAverage-metricalert"
+  resource_group_name = azurerm_resource_group.privatecloud.name
+  scopes              = [module.avs_privatecloud.privatecloud_id]
+  description         = "Usage Average alert per cluster"
+
+  criteria {
+    metric_namespace = "Microsoft.AVS/privateClouds"
+    metric_name      = "UsageAverage"
+    Aggregation      = "Average"
+    operator         = "GreaterThan"
+    threshold        = 80
+
+    dimension {
+      name     = "clustername"
+      operator = "Include"
+      values   = ["*"]
+    }
+  }
+}
